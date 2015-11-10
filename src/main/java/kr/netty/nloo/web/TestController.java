1f952977fae7082aa568df1f893f4fc76e9c41f5
Sukmin Kwon <killroad@gmail.com>
2015-11-10 23:32:06 +0900

    log add

--- src/main/java/kr/netty/nloo/web/TestController.java
+++ src/main/java/kr/netty/nloo/web/TestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import kr.netty.nloo.model.Test;
 import kr.netty.nloo.service.TestService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 	
+	private static Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@Autowired
 	private TestService testService;
 	
 	@RequestMapping("/test")
 	@ResponseBody
 	public List<Test> test(){
+		logger.info("i need money");
 		return testService.getAll();
 	}
 
