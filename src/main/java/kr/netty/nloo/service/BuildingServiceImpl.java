6d206a59ab23786516b25cb476a43e42a792f72e
Sukmin Kwon <killroad@gmail.com>
2015-11-16 14:42:26 +0900

    ¾ø´Â °ǹ° Á¶ȸ½Ã ÀͼÁ¼Ç ¹߻ý ó¸®

--- src/main/java/kr/netty/nloo/service/BuildingServiceImpl.java
+++ src/main/java/kr/netty/nloo/service/BuildingServiceImpl.java
@@ -1,6 +1,10 @@
 package kr.netty.nloo.service;
 
+import java.util.Objects;
+
 import org.apache.ibatis.javassist.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +14,19 @@ import kr.netty.nloo.repository.BuildingRepository;
 @Service
 public class BuildingServiceImpl implements BuildingService {
 	
+	private static final Logger logger = LoggerFactory.getLogger(BuildingServiceImpl.class);
+	
 	@Autowired
 	private BuildingRepository buildingRepository;
 
 	@Override
 	public BuildingViewInfo getViewInfo(Long buildingSequence) throws NotFoundException {
-		return buildingRepository.selectViewInfo(buildingSequence);
+		
+		BuildingViewInfo viewInfo = buildingRepository.selectViewInfo(buildingSequence);
+		if(Objects.isNull(viewInfo)){
+			throw new NotFoundException(buildingSequence + "는 존재하지 않는 건물입니다.");
+		}
+		return viewInfo;
 	}
 
 }
