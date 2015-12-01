6222e041912379cf7752ecbf0ae578a667673b64
jhlee1007 <jhlee1007@gmail.com>
2015-12-01 14:33:34 +0900

    minor change - delete message propertie file

--- src/main/java/kr/netty/nloo/config/DataSourceConfig.java
+++ src/main/java/kr/netty/nloo/config/DataSourceConfig.java
@@ -10,7 +10,6 @@ import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:properties/common.properties")
-@PropertySource("classpath:properties/messages.properties")
 public class DataSourceConfig {
 
 	@Autowired
